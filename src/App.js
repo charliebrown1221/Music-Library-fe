005dbf0e6ac4cf5b1958407b3dd9020a9639604f
charliebrown1221 <chaimbrown1@yahoo.com>
2022-06-21 13:51:28 -0400

    setup components+ testing functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-
+import React,{useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+
+    const [songs, setSongs]= useState([]);
+
+       
+    useEffect(()=>{
+      getAllsongs();
+    },[])
+
+    async function getAllsongs() {
+      let response = await axios.get('http://127.0.0.1:8000/song/')
+      setSongs(response.data)
+      console.log(response)
+    }
+
+
+
+
+
   return (
     <div>
       <header >
