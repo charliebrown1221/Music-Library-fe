0ec6a1e0d85ed17a2ff3fe4eadf12e6bd06cfbce
charliebrown1221 <chaimbrown1@yahoo.com>
2022-06-22 14:43:54 -0400

    set up filter

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,37 +1,40 @@
 import React, { useState } from 'react';
 
 
-
 const SearchBar = (props) => {
     const [fSong, setFSong]=useState('')
       
     const FilterSong = (data) =>  {
       let  results = data.filter((item)  => {
-          if (item.title === fSong){
-              return true;
-        }  else (item.artist === fSong){
-            return true;
-        }
-        
-         return results;
+          if (item.title === fSong||item.artist === fSong||item.album === fSong||item.genre === fSong||item.release_date === fSong) {
+              return true;}
+         
+         
+         
       })
-      console.logs("filtered song data", results)
+      console.log("filtered song data", results)
+      return results;
       
-
-
+    }
+      
+      function handelFilter(event){
+        event.preventDefault();
+  
+        console.log();
+        FilterSong(props.songs)
+    
+      
     }
 
-      
-
-    
     return (
-      <div>   
-       <form>
+        
+       <form onSubmit={handelFilter}>
+        <div> 
       <input type='text' value={fSong} onChange={(event)=> setFSong(event.target.value)} ></input>
-      </form>
-      <button>Search</button>
-        </div>
+      </div>
+      <button type='submit'>Search</button>
+        </form>
      );
 }
  
-export default SearchBar   ;
+export default SearchBar;
